603dfe54579454612faa10dfc244d50a346b5813
emmafattori <emmalfattori@gmail.com>
2021-03-04 10:36:05 -0500

    adding in semantic ui stylesheet

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items =[
+	{
+		title: 'What is React?', 
+		content: 'React is a front end Javascript framework'
+	}, 
+	{
+		title: 'Why use React?', 
+		content: 'React is a favourite JS library among engineers'
+	}, 
+	{
+		title: 'How do you use React?', 
+		content: 'You use React by creating components'
+	}
+]
+export default () => {
+	return (
+		<div>
+			<br />
+			<Accordion items={items}/>
+		</div>
+	)
+}
