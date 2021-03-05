27e83012a38da46f99cf52b6d9906d6db76deadf
emmafattori <emmalfattori@gmail.com>
2021-03-05 13:24:43 -0500

    refactoring active class to only appear on click

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,15 +11,15 @@ const Accordion = ({ items }) => {
 	};
 
 	const renderedItems = items.map((item, index) => {
-		return (
+		const active = index === activeIndex ? 'active' : '';		return (
 			<React.Fragment key={item.title}>
 				<div 
-				className="title active"
+				className={`title ${active}`}
 				onClick={() => onTitleClick(index)}>
 					<i className="dropdown icon"></i>
 					{item.title}	
 				</div>
-				<div className="content active">
+				<div className={`content ${active}`}>
 					<p>{item.content}</p>
 				</div>
 			</React.Fragment>
@@ -28,7 +28,6 @@ const Accordion = ({ items }) => {
 
 	return <div className="ui styled accordion">
 		{renderedItems}
-		<h1>{activeIndex}</h1>
 	</div>
 }
 
