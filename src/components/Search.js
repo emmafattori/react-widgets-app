b809b44347fe73c8d0a6451edd4cc23af5cc70d4
emmafattori <emmalfattori@gmail.com>
2021-03-09 15:53:08 -0500

    adding in useEffect for search widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-	const[term, setTerm] = useState('');
-	return (
-		<div className="ui form">
-			<div class Name="field">
-				<label>Enter Search Term</label>
-				<input 
-					value={term}
-					onChange={e => setTerm(e.target.value)}
-					className="input" />
-			</div>
+	const [term, setTerm] = useState('programming');
+	const [results, setResults] = useState([]);
 
+
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get('http://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query', 
+					list: 'search',
+					origin: '*',
+					format: 'json', 
+					srsearch: term,
+				},
+			});
+
+			setResults(data.query.search);
+		}
+
+		
+
+		search();
+	}, [term]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="right floated content">
+					<a 
+						className="ui button"
+						href={`http://en.wikipedia.org?curid=${result.pageid}`}
+						>Go
+					</a>
+				</div>
+				<div className="content">
+					<div className="header">
+						{result.title}
+					</div>
+					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				</div>
+			</div>
+		)
+	});
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Search Term</label>
+					<input 
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
+						className="input" 
+					/>
+				</div>
+			</div>
+			<div className="ui celled list">{renderedResults}</div>	
 		</div>
+			
 	);
 }
 
